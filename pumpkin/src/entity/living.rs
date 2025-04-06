bc644ae6b8dcad30a83dd20226368b6b5d99f999
Akash Beh <akashsebastianbeh@gmail.com>
2025-04-05 22:29:15 -0700

    Fixed fall damage

--- pumpkin/src/entity/living.rs
+++ pumpkin/src/entity/living.rs
@@ -193,19 +193,20 @@ impl LivingEntity {
             if fall_distance <= 0.0 || dont_damage {
                 return;
             }
+            println!("Fell {fall_distance}");
 
             let safe_fall_distance = 3.0;
             let mut damage = fall_distance - safe_fall_distance;
-            damage = (damage).round();
-            if !self.check_damage(damage) {
-                return;
-            }
+            damage = (damage).ceil();
+            println!("Damaged {damage}");
 
-            self.entity
-                .play_sound(Self::get_fall_sound(fall_distance as i32))
-                .await;
             // TODO: Play block fall sound
-            self.damage(damage, DamageType::FALL).await; // Fall
+            let check_damage = self.damage(damage, DamageType::FALL).await; // Fall
+            if check_damage {
+                self.entity
+                    .play_sound(Self::get_fall_sound(fall_distance as i32))
+                    .await;
+            }
         } else if height_difference < 0.0 {
             let distance = self.fall_distance.load();
             self.fall_distance
